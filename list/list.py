da1e12ad96d8637168fb8656bbb77e87d17fd86c
Davi Vitorino <davitorino26@gmail.com>
2024-04-13 17:00:34 -0300

    feat: added other half of linked list implementation

--- list/list.py
+++ list/list.py
@@ -4,27 +4,35 @@ from list_item import ListItem
 class List:
 
     def __init__(self):
-        self.__first = None
-        self.__last = None
+        self.__first: ListItem | None = None
+        self.__last: ListItem | None = None
+        self.__count = 0
 
     def get_first(self):
-        return self.__first
+        self.__verify_list_empty()
+        return self.__first.value
 
     def get_last(self):
-        return self.__last
-
-    def get_from_pos(self, pos: int):
-        if pos <= 0:
-            raise Exception('Invalid position')
         self.__verify_list_empty()
-        if pos == 1:
-            return self.__first
-        counter = 2
+        return self.__last.value
+
+    def get_by_pos(self, pos: int):
+        self.__verify_list_empty()
+        self.__verify_position(pos)
         actual = self.__first
-        while actual.next:
-            if pos == counter:
-                return actual.next
-        raise Exception('Position out of range')
+        for cur_pos in range(1, pos+1):
+            if cur_pos == pos:
+                return actual.value
+            actual = actual.next
+
+    def get_by_value(self, value):
+        self.__verify_list_empty()
+        actual = self.__first
+        for _ in range(self.__count):
+            if actual.value == value:
+                return actual
+            actual = actual.next
+        raise Exception('Value not found')
 
     def insert_first(self, value):
         item = ListItem(value)
@@ -32,6 +40,7 @@ class List:
         self.__first = item
         if self.__last is None:
             self.__last = item
+        self.__count += 1
 
     def insert_last(self, value):
         item = ListItem(value)
@@ -40,31 +49,83 @@ class List:
         else:
             self.__last.next = item
         self.__last = item
+        self.__count += 1
+
+    def insert_by_pos(self, pos: int, value):
+        self.__verify_position(pos)
+        if pos == 1:
+            self.insert_first(value)
+            return
+        item = ListItem(value)
+        actual = self.__first
+        for cur_pos in range(1, pos+1):
+            if cur_pos+1 == pos:
+                item.next = actual.next
+                actual.next = item
+                self.__count += 1
+                if item.next is None:
+                    self.__last = item
+            actual = actual.next
 
     def remove_first(self):
         self.__verify_list_empty()
+        first = self.__first
         self.__first = self.__first.next
         if self.__first is None:
             self.__last = None
+        self.__count -= 1
+        return first.value
 
-    def remove_from_pos(self, pos: int):
-        if pos <= 0:
-            raise Exception('Invalid position')
+    def remove_last(self):
         self.__verify_list_empty()
+        if self.__first is self.__last:
+            return self.remove_first()
+        actual = self.__first
+        for _ in range(self.__count - 1):
+            if actual.next is self.__last:
+                last = actual.next
+                actual.next = None
+                self.__last = actual
+                self.__count -= 1
+                return last.value
+            actual = actual.next
+
+    def remove_by_pos(self, pos: int):
+        self.__verify_list_empty()
+        self.__verify_position(pos)
         if pos == 1:
             return self.remove_first()
-        counter = 2
         actual = self.__first
-        while actual.next:
-            if counter == pos:
-                actual.next = actual.next.next
-                if actual.next is None:
+        for cur_pos in range(1, pos):
+            if cur_pos+1 == pos:
+                removed = actual.next
+                actual.next = removed.next
+                self.__count -= 1
+                if removed is self.__last:
                     self.__last = actual
-                return
+                return removed.value
             actual = actual.next
-            counter += 1
-        raise Exception('Position out of range')
+
+    def remove_by_value(self, value):
+        self.__verify_list_empty()
+        if self.__first.value == value:
+            return self.remove_first()
+        actual = self.__first
+        for _ in range(self.__count - 1):
+            if actual.next.value == value:
+                removed = actual.next
+                actual.next = removed.next
+                self.__count -= 1
+                if removed is self.__last:
+                    self.__last = actual
+                return removed
+            actual = actual.next
+        raise Exception('Value not found')
 
     def __verify_list_empty(self):
         if self.__first is None:
             raise Exception('List is empty')
+
+    def __verify_position(self, position: int):
+        if position < 1 or position > self.__count:
+            raise Exception('Position out of bounds')
